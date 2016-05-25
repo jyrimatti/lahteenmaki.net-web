1d9592af50e9551cffd87336928e963cff27519d
Jyri-Matti Lähteenmäki <jyri-matti@lahteenmaki.net>
2016-05-25 23:59:56 +0300

    make site build failure not destroy everything

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -eu
 
-./index.html.hs > index.html
-./style.css.hs > style.css
+./index.html.hs > index.html.tmp && mv index.html.tmp index.html
+./style.css.hs > style.css.tmp && mv style.css.tmp style.css
